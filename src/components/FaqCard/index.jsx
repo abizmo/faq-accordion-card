8e3a4141251648ae5448d601ee90a86deb7c22bf
Abián Izquierdo <abizmo@gmail.com>
2022-02-07 16:23:03 +0000

    Add illustration mobile

--- src/components/FaqCard/index.jsx
+++ src/components/FaqCard/index.jsx
@@ -11,7 +11,16 @@ const FaqCard = ({ faqs }) => {
   return (
     <div className="bg-white w-mobw h-mobh mx-auto pb-6u px-3u rounded shadow-2xl">
       <div className="flex flex-col items-center mb-2u pt-16u relative">
-
+        <img
+          alt="woman"
+          className="absolute -top-32 w-3/4 z-10"
+          src={`${process.env.PUBLIC_URL}/images/illustration-woman-online-mobile.svg`}
+        />
+        <img
+          alt="shadow"
+          className="absolute top-1 w-3/4 z-0"
+          src={`${process.env.PUBLIC_URL}/images/bg-pattern-mobile.svg`}
+        />
         <h1 className="text-h1 font-bold">FAQ</h1>
       </div>
       <div>
